81786b9b26efa8b61cb64096e3cd7f8456230c77
BimBimBamBam1337 <a94440003@gmail.com>
2025-10-20 11:18:37 +0300

    feat: test voice handler

--- src/telegram/handlers/voice.py
+++ src/telegram/handlers/voice.py
@@ -1,17 +1,18 @@
 import aiohttp
 import os
-
-from aiogram import Router, F
+from aiogram import Router, F, Bot
 from aiogram.types import Message
-
 from src.client_openai import whisper
 from src.constants import TMP_DIR
 
 router = Router()
 
+# Убедимся, что временная папка есть
+TMP_DIR.mkdir(parents=True, exist_ok=True)
+
 
 @router.message(F.voice)
-async def handle_voice_message(message: Message, bot):
+async def handle_voice_message(message: Message, bot: Bot):
     file = await bot.get_file(message.voice.file_id)
     file_path = file.file_path
 
@@ -24,8 +25,12 @@ async def handle_voice_message(message: Message, bot):
                 with open(local_path, "wb") as f:
                     f.write(await resp.read())
 
+    msg_to_delete = await message.answer("Генерирую ответ...")
     transcription = await whisper.get_transcription(str(local_path))
 
     await message.answer(f"🗣 Распознанный текст:\n\n{transcription}")
+    await bot.delete_message(
+        chat_id=message.chat.id, message_id=msg_to_delete.message_id
+    )
 
     os.remove(local_path)
