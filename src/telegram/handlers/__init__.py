2b5440289c616df3c50a0e0bb26bfc9ccf58a87f
BimBimBamBam1337 <a94440003@gmail.com>
2025-10-20 16:36:41 +0300

    feat: test voice handler

--- src/telegram/handlers/__init__.py
+++ src/telegram/handlers/__init__.py
@@ -4,4 +4,4 @@ from .callback import router as callback_router
 from .message import router as message_router
 from .voice import router as voice_router
 
-routers = [comand_router, callback_router, message_router]
+routers = [comand_router, callback_router, message_router, voice_router]
