336d014eab5d4257d5be579a383395fd45909f1a
BimBimBamBam1337 <a94440003@gmail.com>
2025-10-21 18:32:19 +0300

    feat: test semantic layout

--- src/telegram/handlers/__init__.py
+++ src/telegram/handlers/__init__.py
@@ -3,5 +3,12 @@ from .comand import router as comand_router
 from .callback import router as callback_router
 from .message import router as message_router
 from .voice import router as voice_router
+from .script_generator import router as script_generator_router
 
-routers = [comand_router, callback_router, message_router, voice_router]
+routers = [
+    comand_router,
+    callback_router,
+    message_router,
+    voice_router,
+    script_generator_router,
+]
