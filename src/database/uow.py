b81dc87aa60b24567253250fdd270a344e858431
BimBimBamBam1337 <a94440003@gmail.com>
2025-10-29 16:42:23 +0300

    feat: large update

--- src/database/uow.py
+++ src/database/uow.py
@@ -12,17 +12,25 @@ class UnitOfWork:
         self.promo_code_repo: PromoCodeRepository = PromoCodeRepository(self.session)
         return self
 
-    async def __aexit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_val: BaseException | None,
-        exc_tb: object | None,
-    ) -> None:
-        if exc_type is not None:
-            await self.rollback()
-        else:
-            await self.commit()
-        await self.session.close()
+    # async def __aexit__(
+    #     self,
+    #     exc_type: type[BaseException] | None,
+    #     exc_val: BaseException | None,
+    #     exc_tb: object | None,
+    # ) -> None:
+    #     if exc_type is not None:
+    #         await self.rollback()
+    #     else:
+    #         await self.commit()
+    #     await self.session.close()
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        try:
+            if exc_type is None:
+                await self.session.commit()
+            else:
+                await self.session.rollback()
+        finally:
+            await self.session.close()
 
     async def commit(self):
         await self.session.commit()
