61b2a60beb6bae840069febd6ef8b9b59f0d2371
BimBimBamBam1337 <a94440003@gmail.com>
2025-11-07 14:47:15 +0300

    feat: webhook

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 from aiohttp import web
+from loguru import logger
 from aiogram.types import BotCommand
 from aiogram.webhook.aiohttp_server import setup_application
-from loguru import logger
 
 from src.config import dp, bot, settings
 from src.telegram.midlewares import DependanciesMiddleware
@@ -22,40 +22,36 @@ async def setup_bot_commands():
     )
 
 
-async def on_startup(app: web.Application):
+async def on_startup(app):
     await setup_bot_commands()
-    webhook_url = settings.get_webhook_url
-    await bot.set_webhook(webhook_url, drop_pending_updates=True)
-    logger.info(f"Webhook set to {webhook_url}")
-    me = await bot.get_me()
-    logger.info(f"Bot started as {me.username}")
+    await bot.set_webhook(settings.SITE_URL + "/webhook")
+    logger.info(f"Webhook set to {settings.SITE_URL}")
 
 
-async def on_shutdown(app: web.Application):
+async def on_shutdown(app):
     await bot.delete_webhook()
     logger.info("Webhook deleted")
 
 
-def create_app() -> web.Application:
-    dm = DependanciesMiddleware()
-    dp.message.outer_middleware(dm)
-    dp.callback_query.outer_middleware(dm)
-    dp.include_routers(*routers)
-
+def create_app():
     app = web.Application()
 
     app.router.add_post("/webhook", handle_webhook)
 
     setup_robokassa_routes(app)
 
+    dm = DependanciesMiddleware()
+    dp.message.outer_middleware(dm)
+    dp.callback_query.outer_middleware(dm)
+    dp.include_routers(*routers)
+
     setup_application(app, dp, bot=bot)
 
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
-
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    web.run_app(app, host=settings.site_host, port=settings.site_port)
+    web.run_app(app, host=settings.SITE_HOST, port=settings.SITE_PORT)
